b6c52056e72a84426b8538ef09177cdeaf9faa55
raetzman <raetzman@gmx.de>
2017-09-21 00:22:30 +0200

    removed features was removed

--- views/js/main.js
+++ views/js/main.js
@@ -526,9 +526,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  var count = (Math.floor( height / 256 ) + 1) * 8;
-  var fatherHelper = document.querySelector("#movingPizzas1");
-  for (var i = 0; i < count; i++) {
+  for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza-min.png";
@@ -536,7 +534,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    fatherHelper.appendChild(elem);
+    document.querySelector("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
 });
